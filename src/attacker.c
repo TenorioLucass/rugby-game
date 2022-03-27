dee11ea8457fdd123d8066d7a1a48b8c9c63cd2a
TenorioLucass <lucas.tenorio@usp.br>
2022-03-27 20:21:29 -0300

    Feat: attacker and defender strategy

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -19,11 +20,50 @@
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
   UNUSED(defender_spy);
+  int dir = rand() % 48;
+
+    if(dir == 0){
+      return (direction_t) DIR_STAY;
+    }
+    else if(dir == 1) {
+      return (direction_t) DIR_DOWN_LEFT;
+    }
+    else if(dir == 2) {
+      return (direction_t) DIR_LEFT;
+    }
+    else if(dir == 3) {
+      return (direction_t) DIR_UP_LEFT;
+    }
+    else if(dir >= 4 && dir < 8) {
+      return (direction_t) DIR_UP;
+    }
+    else if(dir >= 8 && dir < 12) {
+      return (direction_t) DIR_DOWN;
+    }
+    else if(dir >= 12 && dir < 24) {
+      return (direction_t) DIR_UP_RIGHT;
+    }
+    else if(dir >= 24 && dir < 36) {
+      return (direction_t) DIR_RIGHT;
+    }
+    else if(dir >= 36 && dir < 48) {
+      return (direction_t) DIR_DOWN_RIGHT;
+    }
+  
 
   // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  return (direction_t) DIR_UP;
 }
 
-/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------
+#define DIR_STAY       {  0,  0 }1
+#define DIR_UP         { -1,  0 }4
+#define DIR_UP_RIGHT   { -1,  1 }12
+#define DIR_RIGHT      {  0,  1 }12
+#define DIR_DOWN_RIGHT {  1,  1 }12
+#define DIR_DOWN       {  1,  0 }4
+#define DIR_DOWN_LEFT  {  1, -1 }1
+#define DIR_LEFT       {  0, -1 }1
+#define DIR_UP_LEFT    { -1, -1 }1
+*/
--- src/defender.c
+++ src/defender.c
@@ -21,9 +21,48 @@ direction_t execute_defender_strategy(
   // TODO: unused parameters, remove these lines later
   UNUSED(defender_position);
   UNUSED(attacker_spy);
-
+  int dir = rand() % 48;
+  
+    if(dir == 0){
+      return (direction_t) DIR_UP_RIGHT;
+    }
+    else if(dir == 1) {
+      return (direction_t) DIR_RIGHT;
+    }
+    else if(dir == 2) {
+      return (direction_t) DIR_DOWN_RIGHT;
+    }
+    else if(dir == 3) {
+      return (direction_t) DIR_LEFT;
+    }
+    else if(dir >= 4 && dir < 8) {
+      return (direction_t) DIR_DOWN_LEFT;
+    }
+    else if(dir >= 8 && dir < 12) {
+      return (direction_t) DIR_UP_LEFT;
+    }
+    else if(dir >= 12 && dir < 24) {
+      return (direction_t) DIR_DOWN;
+    }
+    else if(dir >= 24 && dir < 36) {
+      return (direction_t) DIR_UP;
+    }
+    else if(dir >= 36 && dir < 52) {
+      return (direction_t) DIR_STAY;
+    }
+  
   // TODO: Implement Defender logic here
   return (direction_t) DIR_LEFT;
 }
 
-/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------
+#define DIR_STAY       {  0,  0 }16
+#define DIR_UP         { -1,  0 }12
+#define DIR_UP_RIGHT   { -1,  1 }1
+#define DIR_RIGHT      {  0,  1 }1
+#define DIR_DOWN_RIGHT {  1,  1 }1
+#define DIR_DOWN       {  1,  0 }12
+#define DIR_DOWN_LEFT  {  1, -1 }4
+#define DIR_LEFT       {  0, -1 }1
+#define DIR_UP_LEFT    { -1, -1 }4
+*/
